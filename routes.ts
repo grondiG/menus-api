d0504df1fda5057a3d607e585474f8f58df72fca
Arkadiusz Grondys <arkadiusz.grondys@vality.pl>
2024-01-16 16:30:07 +0100

    Db.json

--- routes.ts
+++ routes.ts
@@ -1,8 +1,49 @@
 import express from 'express'
+import * as db from './db.json';
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.send('Hello world!');
+const dbData = db.restaurants;
+
+router.get('/restaurants', async (req, res) => {
+    try{
+        res.json(dbData);
+    }
+    catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+router.get('/filterRestaurants', async (req, res) => {
+    try{
+        const restaurant = dbData.filter((restaurant: any) => restaurant.name.toLowerCase().startsWith(req.query.name?.toString().toLowerCase()));
+        if(restaurant){
+            res.json(restaurant);
+        }
+        else{
+            res.sendStatus(404);
+        }
+    }
+    catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+router.get('/restaurants/:id', async (req, res) => {
+    try{
+        const restaurant = dbData.find((restaurant: any) => restaurant.id === req.params.id);
+        if(restaurant){
+            res.json(restaurant);
+        }
+        else{
+            res.sendStatus(404);
+        }
+    }
+    catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
 });
 
 export default router;
